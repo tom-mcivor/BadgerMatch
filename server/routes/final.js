f8d8211039ad09ad208dd06af54c9a909b098076
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-11-23 11:45:31 +1300

    patch router functioning to update disposition by id

--- server/routes/final.js
+++ server/routes/final.js
@@ -3,13 +3,30 @@ const router = express.Router()
 
 const db = require('../db/final.js')
 // get  router to have the result
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   db.getFinalResultById(id)
     .then((finalResult) => {
       res.json(finalResult)
     })
-    .catch(console.error)
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
+
+router.patch('/:id', (req, res) => {
+  const id = req.params.id
+  const disposition = req.body.disposition
+  db.addDispositionResult(id, disposition)
+    .then(() => {
+      return db.getFinalResultById(id).then((result) => res.json(result))
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
 })
 
 module.exports = router
