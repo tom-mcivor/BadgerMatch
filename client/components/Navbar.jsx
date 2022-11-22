8d72605e014d83aebe50fc1277299b9ad43eaeb8
Cameron Louw <70700727+cameron-louw@users.noreply.github.com>
2022-11-23 11:30:17 +1300

    add

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.scss'
+import { IfAuthenticated, IfNotAuthenticated } from './isAuthenticated'
+
+// import {Navlink, NavGroup} from './Styled'
+
+import { useAuth0 } from '@auth0/auth0-react'
+
 
 export default function Navbar() {
+
+  const { logout, loginWithRedirect, user } = useAuth0()
+
+  const handleLogOff = (e) => {
+    e.preventDefault()
+    logout()
+  }
+
+  const handleSignIn = (e) => {
+    e.preventDefault()
+    loginWithRedirect()
+  }
+  
   return (
+
+
+  
     <nav className={styles.navbar}>
       <Link to='/'>
         <div className={styles.title}>Badger Match</div>
@@ -12,7 +34,18 @@ export default function Navbar() {
         <Link to='/play'>Play</Link>
         <Link to='/create'>Create</Link>
         <Link to='/results'>Results</Link>
-        <div className={styles.auth}>Register | Login | Log out</div>
+        
+        <IfAuthenticated>
+        <div className={styles.auth}>
+          <div>Hello {user?.nickname}</div>
+          <Link to="/" onClick={handleLogOff}>Log out</Link>
+        </div>
+        </IfAuthenticated>
+
+        <IfNotAuthenticated>
+          <Link className={styles.auth} to="/" onClick={handleSignIn}>Register | Login </Link>
+        </IfNotAuthenticated>
+
       </div>
     </nav>
   )
