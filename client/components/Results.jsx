51fcfd9fd2b3100457f7ca4fc5c3e07af57454e9
Jessie Mills <111710651+jessie-mills-dev@users.noreply.github.com>
2022-11-23 18:10:06 +1300

    Complete Results-Frontend with tests

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -17,25 +17,22 @@ export default function Results() {
       {results.map((result, key) => {
         const date = new Date(`${result?.created}`*1).toLocaleString()
       
-     
         const friendOrFoeColour = ( `${result?.disposition}` == 'friend' ? 'green' : 'red')
-
         const textColour = {
           color: `${friendOrFoeColour}`,
         }
 
       return (
         <div className={styles.resultscontainer} key={key}> 
-        <div className={styles.item1}>
-        <img className={styles.image} src={result?.imageUrl} width={400} alt={result?.name}/>
-        </div>
-        <div className={styles.item2}>
-        <div className={styles.friendorfoe} > Friend or Foe? <div style={textColour}>{result?.disposition.toUpperCase()}</div></div>
-        <div className={styles.name} >My name is {result?.name}</div>
-        <div>We met on: {date}</div>
-        <div>About me: {result?.description}</div>
-        
-        </div>
+          <div className={styles.item1}>
+          <img className={styles.image} src={result?.imageUrl} width={400} alt={result?.name}/>
+          </div>
+          <div className={styles.item2}>
+          <div className={styles.friendorfoe} > Friend or Foe? <div style={textColour}>{result?.disposition.toUpperCase()}</div></div>
+          <div className={styles.name} >My name is {result?.name}</div>
+          <div>We met on: {date}</div>
+          <div>About me: {result?.description}</div>
+          </div>
         </div>
       )}
       )}
---
+++ client/components/__tests__/Results.test.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux'
+import { screen, render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import Results from '../Results'
+import { fetchResults } from '../../actions/results'
+
+const resultContentMockData = [{
+  name: 'Penguin',
+  imageUrl: '/penguin.png',
+  description: `I'm a lovely penguin`,
+  disposition: 'friend'
+},
+{
+  name: 'Puppy',
+  imageUrl: '/puppy.png',
+  description: `I'm a naughty puppy`,
+  disposition: 'foe'
+},
+{
+  name: 'Potato',
+  imageUrl: '/potato.png',
+  description: `I'm a stud I mean spud`,
+  disposition: 'friend'
+}
+]
+
+jest.mock('../../actions/results')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+const fakeStore = {
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+  getState: jest.fn(() => {
+    return { results: resultContentMockData }
+  }),
+}
+
+describe('<Results />', () => {
+  it('displays image, animal name, description and disposition from redux state.', () => {
+    expect.assertions(4)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <Results />
+        </BrowserRouter>
+      </Provider>
+    )
+    const animalName = screen.getAllByText(/penguin/i)
+    expect(animalName[0]).toBeInTheDocument()
+    const animalDescription = screen.getByText(resultContentMockData[1].description, {
+      exact: false,
+    })
+    expect(animalDescription.innerHTML).toContain(`I'm a naughty puppy`)
+    const animalDisposition = screen.getAllByText(resultContentMockData[2].disposition, {
+      exact: false,
+    })
+    expect(animalDisposition[4].innerHTML).toContain('FRIEND')
+    
+    const image = screen.getAllByRole('img')[0]
+    expect(image.src).toMatch('/penguin.png')
+    
+    console.log(animalName)
+    
+  })
+  it('dispatches the fetchResults thunk.', () => {
+    expect.assertions(1)
+    const fetchResultContentMockReturn = () => 'mockReturnFunctionsReturnValue'
+    fetchResults.mockReturnValue(fetchResultContentMockReturn)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <Results />
+        </BrowserRouter>
+      </Provider>
+    )
+    expect(fakeStore.dispatch).toHaveBeenCalledWith(fetchResultContentMockReturn)
+  })
+})
