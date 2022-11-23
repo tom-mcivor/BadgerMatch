8bd72e4f84f2cf125374a6c3977c97b714eea460
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-11-23 16:18:18 +1300

    Results.jsx component working

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react'
 import { fetchResults } from '../actions/results'
 import { useDispatch, useSelector } from 'react-redux'
+import styles from './Results.module.scss'
 
 export default function Results() {
   const dispatch = useDispatch()
   const results = useSelector((state) => state.results)
-
-  console.log(results, 'results');
 
   useEffect(() => {
     dispatch(fetchResults())
@@ -14,11 +13,29 @@ export default function Results() {
   
   return (
     <>
-     <h1>Your Previous Results</h1>
+     <h1 className={styles.resultsheading} >Your Previous Results</h1>
       {results.map((result, key) => {
+        const date = new Date(`${result?.created}`*1).toLocaleString()
+      
+     
+        const friendOrFoeColour = ( `${result?.disposition}` == 'friend' ? 'green' : 'red')
+
+        const textColour = {
+          color: `${friendOrFoeColour}`,
+        }
+
       return (
-        <div key={key}> 
-        <img src={result?.imageUrl} width={400} alt={result?.name}/>
+        <div className={styles.resultscontainer} key={key}> 
+        <div className={styles.item1}>
+        <img className={styles.image} src={result?.imageUrl} width={400} alt={result?.name}/>
+        </div>
+        <div className={styles.item2}>
+        <div className={styles.friendorfoe} > Friend or Foe? <div style={textColour}>{result?.disposition.toUpperCase()}</div></div>
+        <div className={styles.name} >My name is {result?.name}</div>
+        <div>We met on: {date}</div>
+        <div>About me: {result?.description}</div>
+        
+        </div>
         </div>
       )}
       )}
