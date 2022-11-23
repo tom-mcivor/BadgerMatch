86cb631d23f1db335cf1882bb464f1bbe9f927f4
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-11-23 18:04:10 +1300

    add files and apiClient route

---
+++ client/apis/final.js
@@ -0,0 +1,22 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1'
+// can we use '/api/v1/final'?????
+
+//GET /api/v1/final/:id
+export function getAnimalById(id) {
+  return request.get(`${rootUrl}/final/${id}`).then((res) => {
+    console.log(res.body)
+    return res.body
+  })
+}
+
+//POST /api/vi/final
+export function postResult(newResult) {
+  return request
+    .post(`${rootUrl}/final`)
+    .send(newResult)
+    .then((res) => {
+      return res.body
+    })
+}
