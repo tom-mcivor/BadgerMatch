1f30b69a5db4cc4388a158bb9743aa7a8eb766da
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-11-24 12:03:21 +1300

    Auth-Backend route setup with jwt

--- server/routes/results.js
+++ server/routes/results.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const db = require('../db/results.js')
 const router = express.Router()
+const checkJwt = require('../auth0') 
 
 //api/v1/results/
-router.get('/', (req, res) => {
-  // NOTE: auth0id is hardwired for now until auth is set up
-  const auth0id = 1
+router.get('/', checkJwt, (req, res) => {
+  const auth0id = req.auth?.sub
   db.getResult(auth0id)
     .then((result) => {
       res.json(result)
