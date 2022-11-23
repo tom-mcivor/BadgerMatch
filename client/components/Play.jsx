077a85ae11fddf04e5a92da186bf74363fb0b7bb
Gus Gieseg <angus@DESKTOP-MLVH5VI.localdomain>
2022-11-24 11:05:30 +1300

    second round revisions

--- client/components/Play.jsx
+++ client/components/Play.jsx
@@ -1,2 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { getAnimals } from '../apis/play'
+
+import React from 'react'
+
+export default function Play() {
+  return (
+    <>
+      <h1>PLAY PAGE :D</h1>
+    </>
+  )
+}
