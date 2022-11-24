896f127d48c48dd7115bb74086c75f1fe7bf2f81
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-11-24 22:09:27 +1300

    add

---
+++ client/components/__tests__/Final.test.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import Final from '../Final'
+import { fetchAnimal, addResult } from '../../actions/final'
+
+const animalByIdMockData = {
+  id: 1,
+  auth0_id: '1',
+  name: 'Bag Cat',
+  description: 'Likes bags',
+  imageUrl: '/images/bag-cat.jpg',
+}
+
+const resultMockData = {
+  auth0_id: 1,
+  animal_id: 1,
+  created: new Date(Date.now()),
+  disposition: 'friend',
+}
+
+jest.mock('../../actions/final')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+const fakeStore = {
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+  getState: jest.fn(() => {
+    return { final: [animalByIdMockData, resultMockData] }
+  }),
+}
+
+describe('<Final/>', () => {
+  it('display image and animal name from redux state by ID.', () => {
+    expect.assertions(2)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <Final />
+        </BrowserRouter>
+      </Provider>
+    )
+    const animalName = screen.getByText(animalByIdMockData.name, {
+      exact: false,
+    })
+    expect(animalName).toBeTruthy()
+    const image = screen.getByRole('img')
+    expect(image.src).toMatch(animalByIdMockData.imageUrl)
+  })
+  it('dispatch the fetchAnimal thunk', () => {
+    expect.assertions(1)
+    const fetchAnimalContentMockReturn = () => 'mockReturnFunctionsReturnValue'
+    fetchAnimal.mockReturnValue(fetchAnimalContentMockReturn)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <Final />
+        </BrowserRouter>
+      </Provider>
+    )
+    expect(fakeStore.dispatch[0]).toHaveBeenCalledWith(
+      fetchAnimalContentMockReturn
+    )
+  })
+
+  it('save button event to redux state', () => {
+    expect.assertions(1)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <Final />
+        </BrowserRouter>
+      </Provider>
+    )
+    const result = screen.getAllByRole('button')[0]
+    fireEvent.click(result, { target: { value: 'friend' } })
+    const button = screen.getAllByRole('button')
+    fireEvent.click(button)
+    const state = fakeStore.getState()
+    console.log(state)
+    expect(state.final[1]).toEqual(resultMockData.disposition)
+  })
+})
