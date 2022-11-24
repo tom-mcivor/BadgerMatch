0cdde2fdf95ce04e16e0e4fb32e8bd7bdf2cb45c
James Hart <25505178+HartJN@users.noreply.github.com>
2022-11-25 07:23:10 +1300

    remove old code

--- client/apis/create.js
+++ client/apis/create.js
@@ -1,26 +1,5 @@
 import request from 'superagent'
 
-// export const getS3Url = async (token) => {
-//   const response = await request
-//     .get('/api/v1/create/s3Url')
-//     .set('Authorization', `Bearer ${token}`)
-//   return response.body
-// }
-
-// export const fetchUrl = async (url, file) => {
-//   await request.put(url).send(file)
-// }
-
-// export const create = async (animal, token) => {
-//   console.log('token', token)
-//   const response = await request
-//     .post('/api/v1/create')
-//     .set('Authorization', `Bearer ${token}`)
-//     .send(animal)
-
-//   return response.body
-// }
-
 export const getS3Url = async (file, token) => {
   const response = await request
     .get('/api/v1/create/s3Url')
