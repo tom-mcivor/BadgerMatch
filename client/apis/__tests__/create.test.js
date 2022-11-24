4e81c8fbc8ab6f98f167695c01f51ba45b51a95b
James Hart <25505178+HartJN@users.noreply.github.com>
2022-11-25 07:25:13 +1300

    disable old tests

--- client/apis/__tests__/create.test.js
+++ client/apis/__tests__/create.test.js
@@ -2,11 +2,12 @@ import nock from 'nock'
 import { create, getS3Url, fetchUrl } from '../create'
 
 const createContentMockData = {
-  auth0Id: 1,
   name: 'Borris',
   description: 'Likes stuff',
   imageUrl: '/images/boris.jpg',
 }
+
+const fakeToken = 'anything'
 
 const fetchUrlMockData = {
   uploadUrl: 'https://s3.amazonaws.com/random-bucket-name/1234567890.jpeg',
@@ -16,38 +17,37 @@ test('1=1', () => {
   expect(1).toBe(1)
 })
 
-// describe('create', () => {
-//   it('should add a new animal to the database', async () => {
-//     expect.assertions(1)
-//     nock('http://localhost:3000')
-//       .post('/api/v1/create')
-//       .reply(200, createContentMockData)
-//     const res = await create(createContentMockData)
-//     expect(res).toEqual(createContentMockData)
-//   })
-// })
+describe('create', () => {
+  it('should add a new animal to the database', async () => {
+    expect.assertions(2)
+    const scope = nock('http://localhost')
+      .post('/api/v1/create')
+      .reply(200, { ...createContentMockData, token: fakeToken })
+    const res = await create(createContentMockData)
+    expect(res).toEqual({ ...createContentMockData, token: fakeToken })
+    expect(scope.isDone()).toBeTruthy()
+  })
+})
 
+// // TODO: Fix this test
 // describe('getS3Url', () => {
 //   it('should return a uploadUrl', async () => {
-//     expect.assertions(1)
-//     nock('http://localhost:3000')
+//     expect.assertions(2)
+//     const scope = nock('http://localhost')
 //       .get('/api/v1/create/s3Url')
-//       .reply(200, { uploadUrl: 'mockUploadUrl' })
+//       .reply(200, { uploadUrl: 'mockUploadUrl', token: fakeToken })
 //     const res = await getS3Url()
-//     expect(res).toEqual({ uploadUrl: 'mockUploadUrl' })
+//     expect(res).toEqual({ uploadUrl: 'mockUploadUrl', token: fakeToken })
+//     expect(scope.isDone()).toBeTruthy()
+//   })
+
+//   it('should post to the s3Url', async () => {
+//     expect.assertions(2)
+//     const scope = nock('http://localhost')
+//       .post('/api/v1/create/s3Url')
+//       .reply(200, { uploadUrl: 'mockUploadUrl', token: fakeToken })
+//     const res = await getS3Url()
+//     expect(res).toEqual({ uploadUrl: 'mockUploadUrl', token: fakeToken })
+//     expect(scope.isDone()).toBeTruthy()
 //   })
 // })
-
-// // describe('fetchUrl', () => {
-// //   it('should return a uploadUrl', async () => {
-// //     expect.assertions(1)
-// //     nock('http://localhost:3000')
-// //       .get('/api/v1/create/s3Url')
-// //       .reply(200, fetchUrlMockData)
-// //     const res = await fetchUrl(
-// //       fetchUrlMockData.uploadUrl,
-// //       fetchUrlMockData.file
-// //     )
-// //     expect(res).toEqual({ uploadUrl: 'mockUploadUrl' })
-// //   })
-// // })
--- client/components/__tests__/Create.test.jsx
+++ client/components/__tests__/Create.test.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { render, screen, fireEvent, act } from '@testing-library/react'
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react'
 import Create from '../Create'
 import Dropzone from 'react-dropzone'
+import { useAuth0 } from '@auth0/auth0-react'
+import '@testing-library/jest-dom'
+import { create, getS3Url } from '../../apis/create'
 
 jest.spyOn(console, 'log').mockImplementation(() => {})
 jest.spyOn(console, 'error').mockImplementation(() => {})
 
+jest.mock('@auth0/auth0-react')
+jest.mock('../../apis/create')
+
 beforeEach(() => {
-  jest.resetAllMocks()
+  useAuth0.mockReturnValue({
+    getAccessTokenSilently: () => {
+      return Promise.resolve('this-is-a-token')
+    },
+  })
 })
 
 describe('<Create />', () => {
@@ -27,4 +37,148 @@ describe('<Create />', () => {
     expect(descriptionInput.value).toBe('test description')
     expect(fileInput.files[0]).toBe('test file')
   })
+
+  it('displays a loading message when the form is submitted', async () => {
+    render(<Create />)
+    const nameInput = screen.getByTestId('name-input')
+    const descriptionInput = screen.getByTestId('description-input')
+    const fileInput = screen.getByTestId('image-input')
+    const submitButton = screen.getByTestId('submit-button')
+
+    fireEvent.change(nameInput, { target: { value: 'test name' } })
+    fireEvent.change(descriptionInput, {
+      target: { value: 'test description' },
+    })
+    fireEvent.change(fileInput, { target: { files: ['test file'] } })
+
+    fireEvent.click(submitButton)
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument()
+  })
+
+  // it('displays a success message when the form is submitted', async () => {
+  //   render(<Create />)
+  //   const nameInput = screen.getByTestId('name-input')
+  //   const descriptionInput = screen.getByTestId('description-input')
+  //   const fileInput = screen.getByTestId('image-input')
+  //   const submitButton = screen.getByTestId('submit-button')
+
+  //   fireEvent.change(nameInput, { target: { value: 'test name' } })
+  //   fireEvent.change(descriptionInput, {
+  //     target: { value: 'test description' },
+  //   })
+  //   fireEvent.change(fileInput, { target: { files: ['test file'] } })
+
+  //   fireEvent.click(submitButton)
+
+  //   await waitFor(() => {
+  //     expect(screen.getByText('Success!')).toBeInTheDocument()
+  //   })
+  // })
+
+  // it('calls the create api when the form is submitted', async () => {
+  //   render(<Create />)
+  //   const nameInput = screen.getByTestId('name-input')
+  //   const descriptionInput = screen.getByTestId('description-input')
+  //   const fileInput = screen.getByTestId('image-input')
+  //   const submitButton = screen.getByTestId('submit-button')
+
+  //   fireEvent.change(nameInput, { target: { value: 'test name' } })
+  //   fireEvent.change(descriptionInput, {
+  //     target: { value: 'test description' },
+  //   })
+  //   fireEvent.change(fileInput, { target: { files: ['test file'] } })
+
+  //   fireEvent.click(submitButton)
+
+  //   await waitFor(() => {
+  //     expect(create).toHaveBeenCalledWith(
+  //       {
+  //         name: 'test name',
+  //         description: 'test description',
+  //         imageUrl: 'test image url',
+  //       },
+  //       'this-is-a-token'
+  //     )
+  //   })
+  // })
+
+  // it('calls the getS3Url api when the form is submitted', async () => {
+  //   render(<Create />)
+  //   const nameInput = screen.getByTestId('name-input')
+  //   const descriptionInput = screen.getByTestId('description-input')
+  //   const fileInput = screen.getByTestId('image-input')
+  //   const submitButton = screen.getByTestId('submit-button')
+
+  //   fireEvent.change(nameInput, { target: { value: 'test name' } })
+  //   fireEvent.change(descriptionInput, {
+  //     target: { value: 'test description' },
+  //   })
+  //   fireEvent.change(fileInput, { target: { files: ['test file'] } })
+
+  //   fireEvent.click(submitButton)
+
+  //   await waitFor(() => {
+  //     expect(getS3Url).toHaveBeenCalledWith('test file', 'this-is-a-token')
+  //   })
+  // })
 })
+
+// import React from 'react'
+// import { render, screen, fireEvent, act, waitFor } from '@testing-library/react'
+// import Create from '../Create'
+// import Dropzone from 'react-dropzone'
+// import { useAuth0 } from '@auth0/auth0-react'
+// import '@testing-library/jest-dom'
+// import { create, getS3Url } from '../../apis/create'
+
+// jest.spyOn(console, 'log').mockImplementation(() => {})
+// jest.spyOn(console, 'error').mockImplementation(() => {})
+
+// jest.mock('@auth0/auth0-react')
+// jest.mock('../../apis/create')
+
+// beforeEach(() => {
+//   useAuth0.mockReturnValue({
+//     getAccessTokenSilently: () => {
+//       return Promise.resolve('this-is-a-token')
+//     },
+//   })
+// })
+
+// describe('<Create />', () => {
+//   it('updates the form inputs with user input', async () => {
+//     render(<Create />)
+//     const nameInput = screen.getByTestId('name-input')
+//     const descriptionInput = screen.getByTestId('description-input')
+//     const fileInput = screen.getByTestId('image-input')
+
+//     fireEvent.change(nameInput, { target: { value: 'test name' } })
+//     fireEvent.change(descriptionInput, {
+//       target: { value: 'test description' },
+//     })
+//     fireEvent.change(fileInput, { target: { files: ['test file'] } })
+
+//     expect(nameInput.value).toBe('test name')
+//     expect(descriptionInput.value).toBe('test description')
+//     expect(fileInput.files[0]).toBe('test file')
+//   })
+
+//   it('displays a loading message when the form is submitted', async () => {
+//     render(<Create />)
+//     const nameInput = screen.getByTestId('name-input')
+//     const descriptionInput = screen.getByTestId('description-input')
+//     const fileInput = screen.getByTestId('image-input')
+//     const submitButton = screen.getByTestId('submit-button')
+
+//     fireEvent.change(nameInput, { target: { value: 'test name' } })
+//     fireEvent.change(descriptionInput, {
+//       target: { value: 'test description' },
+//     })
+//     fireEvent.change(fileInput, { target: { files: ['test file'] } })
+
+//     fireEvent.click(submitButton)
+
+//     expect(screen.getByTestId('loading')).toBeInTheDocument()
+//   })
+// })
