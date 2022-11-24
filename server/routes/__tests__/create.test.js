27a0c1dbd132bdd9f1ef58fb265d908d5f0fe626
James Hart <25505178+HartJN@users.noreply.github.com>
2022-11-24 15:56:36 +1300

    fix: apply jwt mock

--- server/routes/__tests__/create.test.js
+++ server/routes/__tests__/create.test.js
@@ -1,35 +1,104 @@
+// const request = require('supertest')
+// const server = require('../../server')
+
+// const { create } = require('../../db/create')
+// jest.spyOn(console, 'error')
+
+// jest.mock('../../db/create')
+
+// afterEach(() => {
+//   console.error.mockReset()
+// })
+
+// describe('POST /api/v1/create', () => {
+//   it('should add a new animal to the database', async () => {
+//     expect.assertions(1)
+//     create.mockImplementation(() => Promise.resolve())
+//     const res = await request(server).post('/api/v1/create').send({
+//       name: 'Bag Cat',
+//       description: 'Likes bags',
+//       imageUrl: '/images/bag-cat.jpg',
+//       auth0Id: 1,
+//     })
+//     expect(res.status).toBe(200)
+//   })
+//   it('should return an error message when the database fails', async () => {
+//     create.mockImplementation(() => Promise.reject('Create Failed'))
+//     const res = await request(server).post('/api/v1/create').send({
+//       name: 'Bag Cat',
+//       description: 'Likes bags',
+//       imageUrl: '/images/bag-cat.jpg',
+//       auth0Id: 1,
+//     })
+//     expect(res.status).toBe(500)
+//   })
+// })
+
+// describe('GET /api/v1/create/s3Url', () => {
+//   it('should return status 200 and a url when successful', async () => {
+//     create.mockImplementation(() => Promise.resolve())
+//     const res = await request(server).get('/api/v1/create/s3Url')
+//     expect(res.status).toBe(200)
+//   })
+//   // it('should return status 500 and an error message when database fails.', async () => {
+//   //   create.mockImplementation(() => Promise.reject('Get Failed'))
+//   //   const res = await request(server).get('/api/v1/create/s3Url')
+//   //   expect(res.status).toBe(500)
+//   //   expect(console.error).toHaveBeenCalledWith('Get Failed')
+//   //   expect(res.text).toContain('Something went wrong')
+//   // })
+// })
+
+// Path: server/routes/__tests__/create.test.js
+
 const request = require('supertest')
 const server = require('../../server')
-
 const { create } = require('../../db/create')
 jest.spyOn(console, 'error')
+const checkJwt = require('../../auth0')
 
 jest.mock('../../db/create')
+jest.mock('../../auth0.js')
+
+beforeEach(() => {
+  checkJwt.mockImplementation((req, res, next) => {
+    ;(req.user = { sub: '999' }), next()
+  })
+})
 
 afterEach(() => {
   console.error.mockReset()
 })
 
+const fakestUser = { name: 'JV', email: 'JV@iscool.com' }
+const fakeUser = { ...fakestUser, auth0_id: '999' }
+
 describe('POST /api/v1/create', () => {
   it('should add a new animal to the database', async () => {
     expect.assertions(1)
     create.mockImplementation(() => Promise.resolve())
-    const res = await request(server).post('/api/v1/create').send({
-      name: 'Bag Cat',
-      description: 'Likes bags',
-      imageUrl: '/images/bag-cat.jpg',
-      auth0Id: 1,
-    })
+    const res = await request(server)
+      .post('/api/v1/create')
+
+      .send({
+        name: 'Bag Cat',
+        description: 'Likes bags',
+        imageUrl: '/images/bag-cat.jpg',
+        auth0Id: 1,
+      })
     expect(res.status).toBe(200)
   })
   it('should return an error message when the database fails', async () => {
     create.mockImplementation(() => Promise.reject('Create Failed'))
-    const res = await request(server).post('/api/v1/create').send({
-      name: 'Bag Cat',
-      description: 'Likes bags',
-      imageUrl: '/images/bag-cat.jpg',
-      auth0Id: 1,
-    })
+    const res = await request(server)
+      .post('/api/v1/create')
+
+      .send({
+        name: 'Bag Cat',
+        description: 'Likes bags',
+        imageUrl: '/images/bag-cat.jpg',
+        auth0Id: 1,
+      })
     expect(res.status).toBe(500)
   })
 })
@@ -38,6 +107,7 @@ describe('GET /api/v1/create/s3Url', () => {
   it('should return status 200 and a url when successful', async () => {
     create.mockImplementation(() => Promise.resolve())
     const res = await request(server).get('/api/v1/create/s3Url')
+
     expect(res.status).toBe(200)
   })
   // it('should return status 500 and an error message when database fails.', async () => {
